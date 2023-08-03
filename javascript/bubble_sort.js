586af04bf27343960b8f5d2d4c3ce1bf25e6a66f
Isaac <isaac.lin.pnc@gmail.com>
2023-08-03 11:14:06 -0400

    finished lav

--- javascript/bubble_sort.js
+++ javascript/bubble_sort.js
@@ -1,5 +1,14 @@
 function bubbleSort(arr) {
-  // type your code here
+  while (arr !== arr.sort()) {
+    for ( let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]){
+        let temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+      }
+    }
+  }
+  return arr
 }
 
 if (require.main === module) {
